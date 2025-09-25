27b07ee2285716e86d6cdb0182472973cbb114e1
Jesse <jessejamesjj007@gmail.com>
2025-09-25 13:17:04 +0300

    Make Initial React Components

---
+++ client/src/components/GuestSignIn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GuestSignIn = () => {
+  return (
+    <div>GuestSignIn</div>
+  )
+}
+
+export default GuestSignIn
---
+++ client/src/components/HotelAdminSignIn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HotelAdminSignIn = () => {
+  return (
+    <div>HotelAdminSignIn</div>
+  )
+}
+
+export default HotelAdminSignIn
