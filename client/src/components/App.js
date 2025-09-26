84cecaa5e5dca667418fd2ead22c8729772bc26e
Jesse <jessejamesjj007@gmail.com>
2025-09-26 18:43:53 +0300

    Setup initial login/checksession/logout for gueste

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
-import GuestHeader from "./GuestHeader";
+import GuestHomePage from "./GuestHomePage";
 
 function App() {
+  const [guest, setGuest] = useState({})
+  const [bookings, setBookings] = useState([])
+  
+  useEffect(() =>{
+    fetch('/guests/id')
+    .then((r) => r.json())
+    .then((data) => setGuest(data))
+  }, [])
+
+  useEffect(()=>{
+    fetch('/bookings/guest/id')
+    .then(r => r.json())
+    .then(data => setBookings(data))
+  })
+
   return(
     <div>
       <h1>Hotel Booking Management App</h1>
+      <GuestHomePage 
+        guest = {guest}
+        bookings = {bookings}
+      />
     </div>
   )
 }
